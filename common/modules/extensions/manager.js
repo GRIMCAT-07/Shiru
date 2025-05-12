342fff5c814d63510b84e3a82535d180beb023ce
RockinChaos <github@CraftationGaming.com>
2025-05-12 12:54:21 -0700

    fix: initialize duplicate workers

--- common/modules/extensions/manager.js
+++ common/modules/extensions/manager.js
@@ -75,7 +75,7 @@ class ExtensionManager {
                 else if (value.sourcesNew) {
                     sources = value.sourcesNew
                     this.whenReady = this.updateExtensions(value.sourcesNew).then((update) => this.loadExtensions(value.sourcesNew, update))
-                    debug('Found new sources and updated...', value.sourcesNew)
+                    debug('Found new sources and updated...', JSON.stringify(value.sourcesNew))
                 }
             }
         })
@@ -98,6 +98,7 @@ class ExtensionManager {
 
     reloadExtensions() {
         Object.values(this.activeWorkers).forEach(worker => worker.terminate())
+        Object.values(this.inactiveWorkers).forEach(worker => worker.terminate())
         this.activeWorkers = {}
         this.whenReady = this.loadExtensions(settings.value.sourcesNew)
     }
@@ -111,6 +112,9 @@ class ExtensionManager {
                     if (this.activeWorkers[key]) {
                         this.activeWorkers[key].terminate()
                         delete this.activeWorkers[key]
+                    } else if (this.inactiveWorkers[key]) {
+                        this.inactiveWorkers[key].terminate()
+                        delete this.inactiveWorkers[key]
                     }
                     delete sourcesNew[_key]
                 }
@@ -119,47 +123,60 @@ class ExtensionManager {
         })
     }
 
+    pending = new Map()
     async addSource(url) {
-        const config = await getManifest(url)
-        if (!config) return `Failed to load extension(s) from the provided source '${url}': ${status.value !== 'offline' ? 'the source is not valid.' : 'no network connection!'}`
-        for (const extension of config) {
-            if (!this.validateConfig(extension)) {
-                await printError('Invalid extension format', `Invalid extension config: ${url}`, { message: `Invalid extension config: ${url} ${JSON.stringify(extension)}`})
-                return `Failed to load extension(s) from '${url}': invalid extension format.`
+        if (this.pending.has(url)) return this.pending.get(url)
+        const promise = (async () => {
+            const config = await getManifest(url)
+            if (!config) {
+                await printError('Failed to load source', `${url}: ${status.value !== 'offline' ? 'the source is not valid.' : 'no network connection!'}`, { message: `Failed to load source: ${url} ${status.value !== 'offline' ? 'the source is not valid.' : 'no network connection!'} ${JSON.stringify(extension)}`})
+                this.pending.delete(url)
+                return `Failed to load extension(s) from the provided source '${url}': ${status.value !== 'offline' ? 'the source is not valid.' : 'no network connection!'}`
             }
-        }
-        settings.update((value) => {
-            const sourcesNew = { ...value.sourcesNew }
-            const extensionsNew = { ...value.extensionsNew }
-            config.forEach(extension => {
-                sourcesNew[extension.id] = extension
-                const key = (extension.locale || (extension.update + '/')) + extension.id
-                if (!extensionsNew[key]) extensionsNew[key] = { enabled: true }
+            for (const extension of config) {
+                if (!this.validateConfig(extension)) {
+                    await printError('Invalid extension format', `Invalid extension config: ${url}`, { message: `Invalid extension config: ${url} ${JSON.stringify(extension)}`})
+                    this.pending.delete(url)
+                    return `Failed to load extension(s) from '${url}': invalid extension format.`
+                }
+            }
+            settings.update((value) => {
+                const sourcesNew = { ...value.sourcesNew }
+                const extensionsNew = { ...value.extensionsNew }
+                config.forEach(extension => {
+                    const key = (extension.locale || (extension.update + '/')) + extension.id
+                    sourcesNew[key] = extension
+                    if (!extensionsNew[key]) extensionsNew[key] = { enabled: true }
+                })
+                return { ...value, sourcesNew, extensionsNew }
             })
-            return { ...value, sourcesNew, extensionsNew }
-        })
+            this.pending.delete(url)
+        })()
+        this.pending.set(url, promise)
+        return promise
     }
 
     async loadExtensions(extensions, update) {
         const extensionIds = Object.keys(extensions || {})
         if (!extensionIds?.length) return false
-        const cachedModules = !update && await Promise.all(extensionIds.map((id) => cache.cachedEntry(caches.EXTENSIONS, (extensions[id]?.locale || (extensions[id]?.update + '/')) + extensions[id]?.id, true)))
-
-        const modules = {}
-        for (const [index, extensionId] of extensionIds.entries()) {
-            const cachedModule = cachedModules?.[index]
-            const key = (extensions[extensionId]?.locale || (extensions[extensionId]?.update + '/')) + extensions[extensionId]?.id
-            if (cachedModule) modules[key] = cachedModule
-            else {
-                const newCode = await getExtension(extensions[extensionId]?.name || extensions[extensionId]?.id, (extensions[extensionId]?.locale || (extensions[extensionId]?.update + '/')) + extensions[extensionId]?.main)
-                modules[key] = (newCode && !extensions[extensionId].locale ? await cache.cacheEntry(caches.EXTENSIONS, key, { mappings: true }, newCode, Date.now() + getRandomInt(7, 14) * 24 * 60 * 60 * 1000) : newCode) || cache.cachedEntry(caches.EXTENSIONS, key, true)
+        const modules = !update ? Object.fromEntries(await Promise.all(extensionIds.map(async (id) => {
+            const cachedModule = await cache.cachedEntry(caches.EXTENSIONS, (extensions[id]?.locale || (extensions[id]?.update + '/')) + extensions[id]?.id, true)
+            return cachedModule ? [id, cachedModule] : null
+        })).then(results => results.flatMap(result => result ? [result] : []))) : {}
+        for (const key of extensionIds) {
+            if (!modules[key]) {
+                const newCode = await getExtension(extensions[key]?.name || extensions[key]?.id, (extensions[key]?.locale || (extensions[key]?.update + '/')) + extensions[key]?.main)
+                modules[key] = (newCode && !extensions[key].locale ? await cache.cacheEntry(caches.EXTENSIONS, key, { mappings: true }, newCode, Date.now() + getRandomInt(7, 14) * 24 * 60 * 60 * 1000) : newCode) || cache.cachedEntry(caches.EXTENSIONS, key, true)
                 if (!modules[key]) continue
             }
 
             if (!this.activeWorkers[key]) {
                 try {
-                    if (this.inactiveWorkers[key]) this.inactiveWorkers[key].terminate()
-                    const worker = createWorker(extensions[extensionId])
+                    if (this.inactiveWorkers[key]) {
+                        this.inactiveWorkers[key].terminate()
+                        delete this.inactiveWorkers[key]
+                    }
+                    const worker = createWorker(extensions[key])
                     try {
                         /** @type {comlink.Remote<import('@/modules/extensions/worker.js').Worker>} */
                         const remoteWorker = await wrap(worker)
@@ -186,17 +203,16 @@ class ExtensionManager {
         if (!extensionIds?.length) return false
 
         const latestManifests = await Promise.all(Object.values(currentExtensions || {}).map(ext => ext?.locale || ext?.update).filter(url => url).map(url => getManifest(url)))
-        const latestValid = Object.fromEntries(latestManifests.flat().filter(config => this.validateConfig(config) && extensionIds.includes(config.id)).map(config => [config.id, config]))
-        const toUpdate = extensionIds.filter(id => latestValid[id]?.version !== currentExtensions[id]?.version)
+        const latestValid = Object.fromEntries(latestManifests.flat().filter(config => this.validateConfig(config) && extensionIds.includes((config.locale || (config.update + '/')) + config.id)).map(config => [((config.locale || (config.update + '/')) + config.id), config]))
+        const toUpdate = extensionIds.filter(id => latestValid?.length && currentExtensions?.length && latestValid[id]?.version !== currentExtensions[id]?.version)
         if (toUpdate.length) {
             settings.update((value) => {
                 const sourcesNew = { ...value.sourcesNew }
                 toUpdate.forEach(id => {
                     try {
-                        const key = (latestValid[id]?.locale || (latestValid[id]?.update + '/')) + id
-                        if (this.activeWorkers[key] && latestValid[id]) {
-                            this.activeWorkers[key].terminate()
-                            delete this.activeWorkers[key]
+                        if (this.activeWorkers[id] && latestValid[id]) {
+                            this.activeWorkers[id].terminate()
+                            delete this.activeWorkers[id]
                         }
                     } catch (error) {
                         debug('Failed to terminate active workers during update, how did we even get here...?')
