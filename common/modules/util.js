afe2078a3fa2112d710a812ba60186c85d98e9a9
RockinChaos <github@CraftationGaming.com>
2025-05-26 23:02:51 -0700

    fix: null string

--- common/modules/util.js
+++ common/modules/util.js
@@ -313,7 +313,7 @@ export function deepEqual(a, b) {
 }
 
 export function capitalize(str) {
-  return str.replace(/\b\w/g, char => char.toUpperCase())
+  return str?.replace(/\b\w/g, char => char.toUpperCase())
 }
 
 export function toFlags(code) {
