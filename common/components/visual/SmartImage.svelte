e7ba03d34218d40d375b2f95577d0a99a3b03355
RockinChaos <github@CraftationGaming.com>
2025-08-21 16:53:21 -0700

    fix: showing broken image

--- common/components/visual/SmartImage.svelte
+++ common/components/visual/SmartImage.svelte
@@ -1,26 +1,28 @@
 <script>
-    export let images = []
-    export let hidden = false
+  export let images = []
+  export let hidden = false
 
-    let index = 0
-    let resolvedImages = []
-    $: filteredImages = images.filter(Boolean)
-    $: loadNextImage(index, filteredImages)
-    async function loadNextImage(index, filteredImages) {
-        let image = filteredImages[index]
-        try {
-            if (typeof image === 'function') image = image()
-            if (image && typeof image.then === 'function') image = await image
-            if (Array.isArray(image)) {
-                filteredImages.splice(index, 1, ...image.filter(Boolean))
-                image = filteredImages[index]
-            }
-        } catch { image = '404.jpg' }
-        resolvedImages[index] = image
+  let index = 0
+  let resolvedImages = []
+  $: filteredImages = images.filter(Boolean)
+  $: loadNextImage(index, filteredImages)
+  async function loadNextImage(index, filteredImages) {
+    let image = filteredImages[index]
+    try {
+      if (typeof image === 'function') image = image()
+      if (image && typeof image.then === 'function') image = await image
+      if (Array.isArray(image)) {
+        filteredImages.splice(index, 1, ...image.filter(Boolean))
+        image = filteredImages[index]
+      }
+    } catch { image = `${index}_404.jpg` }
+    resolvedImages[index] = image
+  }
+  function handleError() {
+    if (index < filteredImages.filter(Boolean).length - 1) {
+      index += 1
     }
-    function handleError() {
-        if (index < filteredImages.length - 1) index += 1
-        else hidden = true
-    }
+    else hidden = true
+  }
 </script>
-<img class={`${$$restProps.class}`} class:d-none={hidden} src={!hidden ? (resolvedImages[index] || '404.jpg') : '404.jpg'} alt='preview' draggable='false' loading='lazy' on:error={handleError} />
+<img class={`${$$restProps.class}`} class:d-none={hidden} src={!hidden ? (resolvedImages[index] || `${index}_404.jpg`) : `${index}_404.jpg`} alt='preview' draggable='false' loading='lazy' on:error={handleError} />
